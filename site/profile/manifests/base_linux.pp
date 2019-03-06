71eebcf4accc4fd3d3de443a799063d5d4cd5853
md <mihkal@dunfjeld.no>
2019-03-06 17:11:52 +0100

    Revert "Reduce complexity"
    This reverts commit cd9f85123e92bda75550a0d13e4072c21706dba2.

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -2,12 +2,15 @@ class profile::base_linux {
 
   class { 'ntp':
     servers  => [ 'ntp.ntnu.no' ],
+    restrict => [
+      'default kod nomodify notrap nopeer noquery',
+      '-6 default kod nomodify notrap nopeer noquery',
+    ],
   }
 
   class { 'timezone':
     timezone => 'Europe/Oslo',
   }
-
   package { 'fortune-mod':
     ensure => present,
   }
