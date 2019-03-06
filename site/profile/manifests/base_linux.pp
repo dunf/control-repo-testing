cd9f85123e92bda75550a0d13e4072c21706dba2
md <mihkal@dunfjeld.no>
2019-03-06 17:03:22 +0100

    Reduce complexity

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -2,15 +2,12 @@ class profile::base_linux {
 
   class { 'ntp':
     servers  => [ 'ntp.ntnu.no' ],
-    restrict => [
-      'default kod nomodify notrap nopeer noquery',
-      '-6 default kod nomodify notrap nopeer noquery',
-    ],
   }
 
   class { 'timezone':
     timezone => 'Europe/Oslo',
   }
+
   package { 'fortune-mod':
     ensure => present,
   }
